d66799c0c1cf0a8d1752a77d9c46a5c714863d90
thravi <thravi@gmail.com>
2017-06-20 13:43:44 -0700

    Update init.pp

--- site/review/manifests/init.pp
+++ site/review/manifests/init.pp
@@ -21,9 +21,6 @@ class review ($user = 'review',) {
     mode   => '0644',
     source => 'puppet:///modules/review/bashrc'
   }
-
-  # add the proper resource to ensure that the Puppet agent is not running
-  # in the background. How would you discover the service name?
   
   service { 'puppet':
     ensure  => stopped,
