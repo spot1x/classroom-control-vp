8e1c6d93eef7014c177aee3d41c7f6aa8874f9ad
spot1x <spot1x@yahoo.com>
2017-06-20 16:12:34 -0700

    Update hosts.pp

--- site/system/manifests/hosts.pp
+++ site/system/manifests/hosts.pp
@@ -5,7 +5,7 @@ purge => true,
 host { 'master.puppetlabs.vm':
 ensure => present,
 host_aliases => ['master'],
-ip => '172.17.0.1', ## use the classroom IP
+ip => '172.17.0.1',
 }
 host { 'localhost':
 ensure => present,
@@ -18,7 +18,7 @@ host_aliases => [
 ],
 ip => '127.0.0.1',
 }
-## Use your own IP, or the ::ipaddress fact
+
 host { 'spot1x.puppetlabs.vm':
 ensure => present,
 host_aliases => ['spot1x'],
