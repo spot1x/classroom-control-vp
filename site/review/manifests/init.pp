600f4b4633126eb743687aef7b56e3f9779df0eb
curibe64 <carlos.g.uribe@jpmchase.com>
2017-06-20 15:39:09 -0500

    Update init.pp

--- site/review/manifests/init.pp
+++ site/review/manifests/init.pp
@@ -4,6 +4,8 @@ $user = 'review',
   # this class should accept a parameter rather than having
   # the username hardcoded.
 
+  include review::motd
+  
   # Uncomment and use this variable where appropriate
 $homedir = $user ? {
     'root'  => '/root',
