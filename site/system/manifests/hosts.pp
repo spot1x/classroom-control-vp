0b5ece90df00e1f5d1be8b31953f3968ff67ec80
justinsanciangco <justin.sanciangco@gmail.com>
2017-06-20 15:52:57 -0700

    Update hosts.pp

--- site/system/manifests/hosts.pp
+++ site/system/manifests/hosts.pp
@@ -1,4 +1,44 @@
 class system::hosts {
   # paste the output of `puppet resource host` in this class
   # and then purge unmanaged resources
+  resource {'host':
+    purge => true,
+  }
+host { 'ip6-allnodes':
+  ensure => 'present',
+  ip     => 'ff02::1',
+  target => '/etc/hosts',
+}
+host { 'ip6-allrouters':
+  ensure => 'present',
+  ip     => 'ff02::2',
+  target => '/etc/hosts',
+}
+host { 'ip6-localnet':
+  ensure => 'present',
+  ip     => 'fe00::0',
+  target => '/etc/hosts',
+}
+host { 'ip6-mcastprefix':
+  ensure => 'present',
+  ip     => 'ff00::0',
+  target => '/etc/hosts',
+}
+host { 'justinsanciangco.puppetlabs.vm':
+  ensure       => 'present',
+  host_aliases => ['justinsanciangco'],
+  ip           => '172.17.0.8',
+  target       => '/etc/hosts',
+}
+host { 'localhost':
+  ensure => 'present',
+  ip     => '127.0.0.1',
+  target => '/etc/hosts',
+}
+host { 'master.puppetlabs.vm':
+  ensure       => 'present',
+  host_aliases => ['puppet'],
+  ip           => '172.17.0.1',
+  target       => '/etc/hosts',
+}
 }
