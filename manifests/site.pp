d12884ceece73b895fd677a8535f45c62c355a8d
williamkorb <korb@qisc.com>
2017-06-20 12:50:33 -0500

    typo?

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,5 +44,5 @@ node default {
   #   class { 'my_class': }
   include role::classroom
   
-  notify { "This is a test for PE Practitioner!" : }
+  notify { "This is a test for PE Practitioner!": }
 }
