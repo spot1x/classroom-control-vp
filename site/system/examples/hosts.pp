969c2fce2e87529c6d29c87f896f63bce46fc782
LeeLogan777 <Leland.Logan@harris.com>
2017-06-20 15:55:38 -0700

    Update hosts.pp

--- site/system/examples/hosts.pp
+++ site/system/examples/hosts.pp
@@ -1,2 +1,39 @@
 include system::hosts
 
+ host { 'ip6-allnodes':
+         ensure => 'present',
+         ip     => 'ff02::1',
+         target => '/etc/hosts',
+      }
+       host { 'ip6-allrouters':
+         ensure => 'present',
+         ip     => 'ff02::2',
+         target => '/etc/hosts',
+     }
+      host { 'ip6-localnet':
+        ensure => 'present',
+        ip     => 'fe00::0',
+        target => '/etc/hosts',
+     }
+      host { 'ip6-mcastprefix':
+        ensure => 'present',
+        ip     => 'ff00::0',
+        target => '/etc/hosts',
+     }
+      host { 'leelogan777.puppetlabs.vm':
+        ensure       => 'present',
+        host_aliases => ['leelogan777'],
+        ip           => '172.17.0.13',
+        target       => '/etc/hosts',
+     }
+     host { 'localhost':
+        ensure => 'present',
+        ip     => '127.0.0.1',
+        target => '/etc/hosts',
+     }
+      host { 'master.puppetlabs.vm':
+        ensure       => 'present',
+        host_aliases => ['puppet'],
+        ip           => '172.17.0.1',
+        target       => '/etc/hosts',
+     }
