b2a8a2c0ebfbafaa150b1c729b71a45289ff025a
Dean <dtruong@navexglobal.com>
2017-06-20 15:57:30 -0700

    updated hosts.pp to purge all but explicit hosts

--- site/system/manifests/hosts.pp
+++ site/system/manifests/hosts.pp
@@ -1,4 +1,44 @@
 class system::hosts {
+  resources {'host':
+    purge => true,
+  }
   # paste the output of `puppet resource host` in this class
   # and then purge unmanaged resources
+  host { 'ip6-allnodes':
+    ensure => 'present',
+    ip     => 'ff02::1',
+    target => '/etc/hosts',
+  }
+  host { 'ip6-allrouters':
+    ensure => 'present',
+    ip     => 'ff02::2',
+    target => '/etc/hosts',
+  }
+  host { 'ip6-localnet':
+    ensure => 'present',
+    ip     => 'fe00::0',
+    target => '/etc/hosts',
+  }
+  host { 'ip6-mcastprefix':
+    ensure => 'present',
+    ip     => 'ff00::0',
+    target => '/etc/hosts',
+  }
+  host { 'localhost':
+    ensure => 'present',
+    ip     => '127.0.0.1',
+    target => '/etc/hosts',
+  }
+  host { 'master.puppetlabs.vm':
+    ensure       => 'present',
+    host_aliases => ['puppet'],
+    ip           => '172.17.0.1',
+    target       => '/etc/hosts',
+  }
+  host { 'navexdean.puppetlabs.vm':
+    ensure       => 'present',
+    host_aliases => ['navexdean'],
+    ip           => '172.17.0.18',
+    target       => '/etc/hosts',
+  }
 }
