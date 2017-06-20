d14a0b782a55c2b7d20c23419b45c801a2a4aef9
Ben Poliquin <Ben.Poliquin@standard.com>
2017-06-20 14:11:09 -0700

    motd.pp adding file

--- site/review/manifests/motd.pp
+++ site/review/manifests/motd.pp
@@ -1,3 +1,9 @@
 class review::motd {
-
+  file { '/etc/motd':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0664',
+    content => epp('review/motd.pp'),
+  }
 }
