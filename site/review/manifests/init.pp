26d24160b8c0eaebc959cb6ec232d2ab44a256ce
Dean <dtruong@navexglobal.com>
2017-06-20 13:45:29 -0700

    Update init.pp

--- site/review/manifests/init.pp
+++ site/review/manifests/init.pp
@@ -1,8 +1,9 @@
-class review {
-  $user = 'review'
   # this class should accept a parameter rather than having
   # the username hardcoded.
-
+ class review (
+   $user = 'review')
+ {
+ 
   # Uncomment and use this variable where appropriate
   $homedir = $user ? {
     'root'  => '/root',
