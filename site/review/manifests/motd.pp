612cd266358b050fdbd6872d0a2ac6ba34c8947b
thravi <thravi@gmail.com>
2017-06-20 14:20:39 -0700

    Update motd.pp

--- site/review/manifests/motd.pp
+++ site/review/manifests/motd.pp
@@ -1,9 +1,9 @@
 class review::motd {
   file { '/etc/motd.pp':
-    ensure   => file,
-    user     => 'root',
-    group    => 'root',
-    mode     => '0644',
-    contennt => epp('review/motd.pp), 
+    ensure  => file,
+    user    => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => epp('review/motd.pp), 
   }
 }
