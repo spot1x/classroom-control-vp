b76efd59606c0e809267248f242abc5f4d441857
LeeLogan777 <Leland.Logan@harris.com>
2017-06-21 08:40:18 -0700

    Update motd.pp

--- site/review/manifests/motd.pp
+++ site/review/manifests/motd.pp
@@ -1,8 +1,34 @@
-class review::motd (
-file { '/etc/motd':
+class review (
+ $user = 'review',
+ ){
+ include review::motd
+ 
+ $homedir = $user ?{
+ 'root' => '/root',
+ default => "/home/$user",
+ }
+ 
+ user {$user:
+ ensure =>present,
+ shell =>'/bin/bash',
+ managehome =>true,
+ }
+ file { "${homedir}/.bashrc":
+ ensure =>file,
+ owner =>$user
+ group =>$user
+ mode =>'0644',
+ source => 'puppet:///modules/review/bashrc'
+ }
+ file { '/etc/motd':
   ensure => file,
   owner => 'root',
   group => 'root',
   mode => '0644'
   content => epp('review/motd.epp'),
+  
+  service {'puppet':
+  ensure => stopped,
+  enable => false,
+  }
 )
