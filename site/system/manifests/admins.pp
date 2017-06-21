4ab5297aaceb6ef6c27c4f4bdc43687ed37c4fdb
dbkillion <brentkillion@gmail.com>
2017-06-21 13:17:44 -0700

    Update admins.pp

--- site/system/manifests/admins.pp
+++ site/system/manifests/admins.pp
@@ -1,5 +1,20 @@
 class system::admins {
   require mysql::server
+  
+  $retired = ['ralph']
+  $admins = {
+  'brad' => { max_queries_per_hour => '600' }, 
+  'monica' => { max_queries_per_hour => '600' }, 
+  'luke' => { max_queries_per_hour => '600' }, 
+  'zack' => { max_queries_per_hour => '1200' },
+  }
+
+$admins.each |$user, $params| { 
+  mysql_user { "${user}@localhost":
+  ensure => present,
+  max_queries_per_hour => $params['max_queries_per_hour'], 
+  }
+
   mysql_user { 'zack@localhost':
     ensure => present,
     max_queries_per_hour => 1200,
@@ -20,7 +35,8 @@ class system::admins {
     max_queries_per_hour => 600,
   }
 
-  user { ['zack', 'monica', 'ralph', 'brad', 'luke']:
-    ensure => present,
+  user { $user:
+  ensure => present, 
+  managehome => true,
   }
 }
