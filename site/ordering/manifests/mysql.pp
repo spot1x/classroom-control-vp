88dff217ccc12e31f76de6bd43c8b4a98770015b
Dean <dtruong@navexglobal.com>
2017-06-21 14:24:49 -0700

    Update mysql.pp

--- site/ordering/manifests/mysql.pp
+++ site/ordering/manifests/mysql.pp
@@ -3,11 +3,11 @@ class ordering::mysql {
   class { '::mysql::server':
     root_password    => 'strongpassword',
   }
-  contain ordering::mysql::server
+  contain ::mysql::server
 
   class { '::mysql::bindings':
     php_enable  => true,
     perl_enable => true,
   }
-  contain ordering::mysql::bindings
+  contain ::mysql::bindings
 }
