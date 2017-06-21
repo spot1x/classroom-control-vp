4a6352a0ed2fc8443fc7c23ee4f1930c2b98473b
Dean <dtruong@navexglobal.com>
2017-06-21 12:59:35 -0700

    refactoring admins.pp using lamdas

--- site/system/manifests/admins.pp
+++ site/system/manifests/admins.pp
@@ -1,40 +1,29 @@
 class system::admins {
   require mysql::server
   
-  $user_queries = {
+  $admin_users = {
     'zack'    => {max_queries_per_hour => '1200'},
     'monitca' => {max_queries_per_hour => '600'},
     'brad'    => {max_queries_per_hour => '600'},
     'luke'    => {max_queries_per_hour => '600'},
   }
   
-  $mysql_user.each|String $user, $params|{
-    mysql_user{ "${user}@localhost":
-    ensure               => present,
-    max_queries_per_hour => $params['max_queries_per_hour']
+  $retired_users = ['ralph']
+  
+  $admin_users.each|String $user, $params|{
+    mysql_user { "${user}@localhost":
+      ensure               => present,
+      max_queries_per_hour => $params['max_queries_per_hour']
+    user { $user:
+      ensure     => present,
+      managehome => true,
+    }
   }
   
-  mysql_user { 'zack@localhost':
-    ensure => present,
-    max_queries_per_hour => 1200,
-  }
-  mysql_user { 'monica@localhost':
-    ensure => present,
-    max_queries_per_hour => 600,
-  }
-  mysql_user { 'ralph@localhost':
-    ensure => absent,
-  }
-  mysql_user { 'brad@localhost':
-    ensure => present,
-    max_queries_per_hour => 600,
-  }
-  mysql_user { 'luke@localhost':
-    ensure => present,
-    max_queries_per_hour => 600,
-  }
-
-  user { ['zack', 'monica', 'ralph', 'brad', 'luke']:
-    ensure => present,
+  $retired_users.each|String $user|{
+    mysql_user { "${user}@localhost":
+      ensure => absent,
+    user { $user:
+      ensure     => present,
   }
 }
