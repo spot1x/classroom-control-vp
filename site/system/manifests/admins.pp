03958c12a6c3661c4e44a0df11c0c064fe532a91
williamkorb <korb@qisc.com>
2017-06-21 14:52:31 -0500

    switched to iterator approach

--- site/system/manifests/admins.pp
+++ site/system/manifests/admins.pp
@@ -1,26 +1,50 @@
 class system::admins {
   require mysql::server
-  mysql_user { 'zack@localhost':
-    ensure => present,
-    max_queries_per_hour => 1200,
-  }
-  mysql_user { 'monica@localhost':
-    ensure => present,
-    max_queries_per_hour => 600,
-  }
-  mysql_user { 'ralph@localhost':
-    ensure => absent,
-  }
-  mysql_user { 'brad@localhost':
-    ensure => present,
-    max_queries_per_hour => 600,
-  }
-  mysql_user { 'luke@localhost':
-    ensure => present,
-    max_queries_per_hour => 600,
-  }
+  $admins = [
+    {
+      username => "zack",
+      max_queries_per_hour => 1200,
+      active => true,
+    },
+    {
+      username => "monica",
+      max_queries_per_hour => 600,
+      active => true,
+    },
+    {
+      username => "ralph",
+      max_queries_per_hour => undef,
+      active => false,
+    },
+    {
+      username => "brad",
+      max_queries_per_hour => 600,
+      active => true,
+    },
+    {
+      username => "luke",
+      max_queries_per_hour => 600,
+      active => true,
+    },
+  ]
 
-  user { ['zack', 'monica', 'ralph', 'brad', 'luke']:
-    ensure => present,
+  $admins.each | String $username, Hash $attributes | {
+    if $active {
+      mysql_user { "${username}@localhost":
+        ensure => present,
+        max_queries_per_hour => $attributes[max_queries_per_hour],
+      }
+      user { "${username}":
+        ensure => present,
+      }
+    }
+    else {
+      mysql_user { "${username}@localhost":
+        ensure => absent,
+      }
+      user { "${username}":
+        ensure => absent,
+      }
+    }
   }
 }
