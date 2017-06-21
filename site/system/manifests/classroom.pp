2f9d132fae2c792e3cdb247b85f92321c63cb6a8
sc00beyd00 <gstinocher@linuxfoundation.org>
2017-06-21 15:43:10 -0700

    comment changes

--- site/system/manifests/classroom.pp
+++ site/system/manifests/classroom.pp
@@ -1,10 +1,10 @@
 class system::classroom {
   # export a virtual host resource for yourself
-  @@host { $::fqdn:
-    ensure => present,
-    ip => $::ipaddress,
-    tag => 'classroom',
-  }
+  #@@host { $::fqdn:
+  #  ensure => present,
+  #  ip => $::ipaddress,
+  #  tag => 'classroom',
+  #}
 
   # collect all resources from the database (including your own)
   # enforce only those tagged with `classroom`.
