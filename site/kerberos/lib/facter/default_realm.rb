9949a1e432192e273d6b90f847bf757e8655a6df
Dean <dtruong@navexglobal.com>
2017-06-21 15:46:34 -0700

    Update default_realm.rb

--- site/kerberos/lib/facter/default_realm.rb
+++ site/kerberos/lib/facter/default_realm.rb
@@ -1,5 +1,5 @@
 # Write a fact that returns the value of running the command
 # "/bin/awk '/default_realm/{print $NF}' /etc/krb5.conf"
-Facter.add('role') do
+Facter.add('default_realm') do
   setcode "/bin/awk '/default_realm/{print $NF}' /etc/krb5.conf"
 end
