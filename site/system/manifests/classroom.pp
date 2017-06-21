18dc35d8bd70737452698e1b271e22bda2c8a1e3
williamkorb <korb@qisc.com>
2017-06-21 17:07:11 -0500

    Update classroom.pp

--- site/system/manifests/classroom.pp
+++ site/system/manifests/classroom.pp
@@ -2,7 +2,9 @@ class system::classroom {
   # export a virtual host resource for yourself
 
   @@host { $::fqdn:
+    ensure => present,
     ip => $::ipaddress,
+    tag => 'classroom',
   }
   # collect all resources from the database (including your own)
   # enforce only those tagged with `classroom`.
