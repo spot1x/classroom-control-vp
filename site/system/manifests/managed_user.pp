42cceea0c34bc9b45a72b617dc48ce19318e655e
Dean <dtruong@navexglobal.com>
2017-06-21 09:43:41 -0700

    added password param, user, set bash for linux OS

--- site/system/manifests/managed_user.pp
+++ site/system/manifests/managed_user.pp
@@ -1,4 +1,5 @@
 define system::managed_user (
+  $password,
   $home = undef,
 ) {
   if $home {
@@ -17,4 +18,16 @@ define system::managed_user (
   # manage a user called $name and that user's `.bashrc` if they're on Linux
   # This can likely reuse some of the code you wrote for the `review` class.
   # Make sure you update variables or paths as required.
+  user { $name:
+    password   => $password,
+    ensure     => present,
+    managehome => true,
+    home       => $homedir,
+  }
+  
+  if $kernel == 'Linux' {
+    file { "${homedir}/bashrc":
+      ensure => present,
+      source => 'puppet:///modules/system/bashrc',
+  }
 }
