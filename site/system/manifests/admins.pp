7a3c1b5265cfae4b8a7300fd0c61aeb7f33ac42f
Austin Blatt <austinblatt@gmail.com>
2017-06-21 13:25:40 -0700

    Exercise 6.2

--- site/system/manifests/admins.pp
+++ site/system/manifests/admins.pp
@@ -1,26 +1,34 @@
 class system::admins {
   require mysql::server
-  mysql_user { 'zack@localhost':
-    ensure => present,
-    max_queries_per_hour => 1200,
-  }
-  mysql_user { 'monica@localhost':
-    ensure => present,
-    max_queries_per_hour => 600,
-  }
-  mysql_user { 'ralph@localhost':
-    ensure => absent,
-  }
-  mysql_user { 'brad@localhost':
-    ensure => present,
-    max_queries_per_hour => 600,
-  }
-  mysql_user { 'luke@localhost':
-    ensure => present,
-    max_queries_per_hour => 600,
+
+  $active_users = {
+    'zack'   => {max_queries_per_hour            => 1200},
+    'ralph'                           => {active =>  false},
+    'monica'                                     => {},
+    'brad'                                       => {},
+    'luke'                                       => {},
   }
 
-  user { ['zack', 'monica', 'ralph', 'brad', 'luke']:
-    ensure => present,
+  $active_users.each |String $username, Hash $attr| {
+    user { $name:
+      ensure => present
+    }
+
+    if defined($attr['max_queries_per_hour']) {
+      $max_queries_per_hour = $attr['max_queries_per_hour']
+    } else {
+      $max_queries_per_hour = 600
+    }
+
+    if defined($attr['active']) and $attr['active'] {
+      $is_active = absent
+    } else {
+      $is_active = present
+    }
+
+    mysql_user { "${name}@localhost":
+      ensure               => $is_active,
+      max_queries_per_hour => $max_queries_per_hour
+    }
   }
 }
