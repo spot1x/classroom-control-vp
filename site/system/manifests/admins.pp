0d74d8cf9af2eccd73b1298ff523b2f04d6c27a0
Dean <dtruong@navexglobal.com>
2017-06-21 12:49:45 -0700

    Update admins.pp

--- site/system/manifests/admins.pp
+++ site/system/manifests/admins.pp
@@ -1,5 +1,19 @@
 class system::admins {
   require mysql::server
+  
+  $user_queries = {
+    'zack'    => {max_queries_per_hour => '1200'},
+    'monitca' => {max_queries_per_hour => '600'},
+    'brad'    => {max_queries_per_hour => '600'},
+    'luke'    => {max_queries_per_hour => '600'},
+  }
+  
+  $mysql_user.each|String $user, $params|{
+    mysql_user{ "${user}@localhost":
+    ensure               => present,
+    max_queries_per_hour => $params['max_queries_per_hour']
+  }
+  
   mysql_user { 'zack@localhost':
     ensure => present,
     max_queries_per_hour => 1200,
