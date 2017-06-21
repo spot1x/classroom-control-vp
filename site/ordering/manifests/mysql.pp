524ae11b4bb2c934d4973329949fd1331fa572fd
justinsanciangco <justin.sanciangco@gmail.com>
2017-06-21 14:26:53 -0700

    Update mysql.pp

--- site/ordering/manifests/mysql.pp
+++ site/ordering/manifests/mysql.pp
@@ -3,11 +3,12 @@ class ordering::mysql {
   class { '::mysql::server':
     root_password    => 'strongpassword',
   }
-
+  contain ::mysql::server
+  
   class { '::mysql::bindings':
     php_enable  => true,
     perl_enable => true,
   }
-
+  contain ::mysql::bindings
 
 }
