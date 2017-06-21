b72bf03b106eef2d4deef795a4bfdaeda5095f31
Dean <dtruong@navexglobal.com>
2017-06-21 15:07:35 -0700

    add host sharing

--- site/system/manifests/classroom.pp
+++ site/system/manifests/classroom.pp
@@ -1,9 +1,11 @@
 class system::classroom {
   # export a virtual host resource for yourself
-
+  @@host { $::fqdn:
+    ip  => $::ipaddress,
+    tag => 'classroom',
+  }
 
   # collect all resources from the database (including your own)
   # enforce only those tagged with `classroom`.
-
-
+  HOST <<| tag=='classroom' |>>
 }
