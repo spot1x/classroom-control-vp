884bfa8e0c3a750ec63bd92eacae6cfc633c33ea
justinsanciangco <justin.sanciangco@gmail.com>
2017-06-21 15:18:23 -0700

    Update classroom.pp

--- site/system/manifests/classroom.pp
+++ site/system/manifests/classroom.pp
@@ -13,41 +13,35 @@ class system::classroom {
   resources {'host':
     purge => true,
   }
-host { 'ip6-allnodes':
-  ensure => 'present',
-  ip     => 'ff02::1',
-  target => '/etc/hosts',
-}
-host { 'ip6-allrouters':
-  ensure => 'present',
-  ip     => 'ff02::2',
-  target => '/etc/hosts',
-}
-host { 'ip6-localnet':
-  ensure => 'present',
-  ip     => 'fe00::0',
-  target => '/etc/hosts',
-}
-host { 'ip6-mcastprefix':
-  ensure => 'present',
-  ip     => 'ff00::0',
-  target => '/etc/hosts',
-}
-host { 'justinsanciangco.puppetlabs.vm':
-  ensure       => 'present',
-  host_aliases => ['justinsanciangco'],
-  ip           => '172.17.0.8',
-  target       => '/etc/hosts',
-}
-host { 'localhost':
-  ensure => 'present',
-  ip     => '127.0.0.1',
-  target => '/etc/hosts',
-}
-host { 'master.puppetlabs.vm':
-  ensure       => 'present',
-  host_aliases => ['puppet'],
-  ip           => '172.17.0.1',
-  target       => '/etc/hosts',
-}
+  host { 'ip6-allnodes':
+    ensure => 'present',
+    ip     => 'ff02::1',
+    target => '/etc/hosts',
+  }
+  host { 'ip6-allrouters':
+    ensure => 'present',
+    ip     => 'ff02::2',
+    target => '/etc/hosts',
+  }
+  host { 'ip6-localnet':
+    ensure => 'present',
+    ip     => 'fe00::0',
+    target => '/etc/hosts',
+  }
+  host { 'ip6-mcastprefix':
+    ensure => 'present',
+    ip     => 'ff00::0',
+    target => '/etc/hosts',
+  }
+  host { 'localhost':
+    ensure => 'present',
+    ip     => '127.0.0.1',
+    target => '/etc/hosts',
+  }
+  host { 'master.puppetlabs.vm':
+    ensure       => 'present',
+    host_aliases => ['puppet'],
+    ip           => '172.17.0.1',
+    target       => '/etc/hosts',
+  }
 }
