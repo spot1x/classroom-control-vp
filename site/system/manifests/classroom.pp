fdee02ef3c176a9faad88b5716c0e531b54b93ed
justinsanciangco <justin.sanciangco@gmail.com>
2017-06-21 15:07:29 -0700

    Update classroom.pp

--- site/system/manifests/classroom.pp
+++ site/system/manifests/classroom.pp
@@ -1,9 +1,13 @@
 class system::classroom {
   # export a virtual host resource for yourself
-
-
+  @@host { $::fqdn:
+    ensure => present,
+    ip     => $::ipaddress,
+    tag    => 'classroom',
+  }
+    
   # collect all resources from the database (including your own)
   # enforce only those tagged with `classroom`.
-
+  Host<<| tag == 'classroom' |>>
 
 }
