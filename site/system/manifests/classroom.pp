a8924a74d650fd02fec537bf9286d64de9288cd3
williamkorb <korb@qisc.com>
2017-06-21 17:12:21 -0500

    Update classroom.pp

--- site/system/manifests/classroom.pp
+++ site/system/manifests/classroom.pp
@@ -5,6 +5,8 @@ class system::classroom {
     ensure => present,
     ip => $::ipaddress,
     tag => 'classroom',
+    host_aliases => ['williamkorb'],
+    target       => '/etc/hosts',
   }
   # collect all resources from the database (including your own)
   # enforce only those tagged with `classroom`.
