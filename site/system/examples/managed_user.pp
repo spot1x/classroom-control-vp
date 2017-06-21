f05f98c6ee88eb9a492be0f9c205857aebca239c
curibe64 <carlos.g.uribe@jpmchase.com>
2017-06-21 18:23:42 -0500

    Update managed_user.pp

--- site/system/examples/managed_user.pp
+++ site/system/examples/managed_user.pp
@@ -2,10 +2,7 @@
 # Generate your own with the command `openssl passwd -1` if you'd like.
 $password = '$1$HdDw//gC$2VBiQ1x5blLPwNS.G.Iw21'
 
-system::managed_user { ['aaron', 'kaitlin', 'alison']:
+system::managed_user { ['aaron', 'kaitlin', 'alison', 'fidel', 'colin']:
   password => $password,
 }
 
-system::managed_user { ['fidel', 'colin', 'john']:
-  password => $password,
-}
