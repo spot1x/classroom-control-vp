3c6031cb9d29fd9dca49d3dd82e195440d0e1384
Adam <alklein@gmail.com>
2017-06-22 09:56:54 -0700

    Update motd.pp

--- site/files/manifests/motd.pp
+++ site/files/manifests/motd.pp
@@ -2,6 +2,9 @@ define files::motd (
   $message = $name,
   $order   = 10,
 ) {
-  # what resource declaration do we need to add to the `motd` concat resource?
-
+    concat::fragment { "motd fragment: ${name}":
+    target => '/etc/motd',
+    order => $order,
+    content => $message,
+  }
 }
