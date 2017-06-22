9d6da3346f0e20d43c031211b97c8ea81ca5966e
sc00beyd00 <gstinocher@linuxfoundation.org>
2017-06-22 09:06:54 -0700

    Update homedir.rb

--- site/system/lib/puppet/parser/functions/homedir.rb
+++ site/system/lib/puppet/parser/functions/homedir.rb
@@ -1,2 +1,8 @@
 # Write a simple function called homedir that accepts a single string argument.
 # It should return the conventional Linux home directory based on a username
+case user
+when 'root'
+return '/root'
+else
+return "/home/#{user}"
+end
