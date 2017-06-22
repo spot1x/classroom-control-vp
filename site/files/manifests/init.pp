aaa3326863de04cfc2e2707abf55fcf3e1086965
Dean <dtruong@navexglobal.com>
2017-06-22 10:12:23 -0700

    Update init.pp

--- site/files/manifests/init.pp
+++ site/files/manifests/init.pp
@@ -22,6 +22,9 @@ class files {
   }
   # What concat resource is needed for this fragment to work?
   concat { '/etc/motd':
+    owner => 'root',
+    group => 'root',
+    mode  => '0644',
   }
   concat::fragment { 'motd header':
     target  => '/etc/motd',
