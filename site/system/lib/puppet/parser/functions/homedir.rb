512a7adaaadc040b77adc99af03eaa9d39ea6aa8
LeeLogan777 <Leland.Logan@harris.com>
2017-06-22 09:28:11 -0700

    Update homedir.rb

--- site/system/lib/puppet/parser/functions/homedir.rb
+++ site/system/lib/puppet/parser/functions/homedir.rb
@@ -1,6 +1,6 @@
 # Write a simple function called homedir that accepts a single string argument.
 # It should return the conventional Linux home directory based on a username
-Puppet:Parser::Functions:newfunction(:homedir,
+Puppet::Parser::Functions.newfunction(:homedir,
   :type => :rvalue
   :arity => 1,
   :doc => "THis will return a typical Linux Home direcotry of the username/'
