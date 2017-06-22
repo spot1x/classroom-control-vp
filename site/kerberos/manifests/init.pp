a3cb5119e35c7dd29f3d5fdc1908cb9127ed6078
sc00beyd00 <gstinocher@linuxfoundation.org>
2017-06-22 10:44:54 -0700

    Update init.pp

--- site/kerberos/manifests/init.pp
+++ site/kerberos/manifests/init.pp
@@ -1,5 +1,8 @@
 class kerberos {
    # use an augeas resource to manage the default_realm
    # determine the proper context with the augtool shell
-
+   augeas { 'update Kerberos default_realm':
+     context => '/files/etc/krb5.conf/libdefaults',
+     changes => 'default_realm EXAMPLE.COM',
+   }
 }
