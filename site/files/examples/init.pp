b3bf61849e2108b2612eff0dd3a7162ada6df697
dbkillion <brentkillion@gmail.com>
2017-06-22 09:56:17 -0700

    Update init.pp

--- site/files/examples/init.pp
+++ site/files/examples/init.pp
@@ -1,2 +1,6 @@
-include files
-
+   include files
+   # extra credit declaration
+   files::motd { 'maintainer notice':
+    message => "\n\n*** This machine is maintained by Wilson ***\n\n"
+    order => 5,
+   }
