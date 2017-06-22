15fa9bdeb9fc35d8c2bbc59c801a97fac6458d09
Austin Blatt <austinblatt@gmail.com>
2017-06-22 10:39:08 -0700

    Exercise 9.2

--- site/kerberos/manifests/init.pp
+++ site/kerberos/manifests/init.pp
@@ -1,5 +1,8 @@
 class kerberos {
-   # use an augeas resource to manage the default_realm
-   # determine the proper context with the augtool shell
-
+  # use an augeas resource to manage the default_realm
+  # determine the proper context with the augtool shell
+  augeas { 'default_realm':
+    context => '/files/etc/krb5.conf/libdefaults/',
+    changes => 'set default_realm PUPPETLABS.VM',
+  }
 }
