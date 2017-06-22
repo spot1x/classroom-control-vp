26b5ae7edbbb8fd82e9bbf03a1206f2607a765c2
Greg Larkin <greg.larkin@puppet.com>
2017-06-22 10:45:32 -0700

    Update init.pp

--- site/kerberos/manifests/init.pp
+++ site/kerberos/manifests/init.pp
@@ -1,5 +1,10 @@
 class kerberos {
    # use an augeas resource to manage the default_realm
    # determine the proper context with the augtool shell
-
+   augeas { 'update Kerberos default_realm':
+     context => '/files/etc/krb5.conf/libdefaults',
+     changes => [ 'default_realm EXAMPLE.COM',
+                  'foo bar',
+                  'baz quux' ],
+   }
 }
