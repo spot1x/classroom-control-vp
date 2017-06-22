b614b893259681d66f77024e1f928b0a94aa6aa7
thravi <thravi@gmail.com>
2017-06-22 10:47:46 -0700

    Update init.pp

--- site/kerberos/manifests/init.pp
+++ site/kerberos/manifests/init.pp
@@ -2,4 +2,12 @@ class kerberos {
    # use an augeas resource to manage the default_realm
    # determine the proper context with the augtool shell
 
+   augeaus { 'change default_realm in krb.conf using augeaus':
+     context => '/files/etc/krb.conf/libdefaults',
+     changes => [ 'set default_realm EXAMPLE.COM',
+                  'set foo bar',
+                  'set 3rd line',
+                ],
+   }
+
 }
